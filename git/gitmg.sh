2894e2c876edec9621e13a66dc4e7328322f812f
okojj <puchon@gmail.com>
2024-06-10 17:05:35 +0900

    add new

---
+++ git/gitmg.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+GIT_DEV_NM="dev-ojj"
+GIT_MAIN_NM="main"
+
+FROM=$1
+TO=$2
+
+if [[ "$FROM" == "" ]] || [[ "$TO" == "" ]] ; then
+	echo "Usage: $(basename $0) from_branch to_branch"
+	echo "ex) $(basename $0) dev-ojj main"
+	exit
+fi
+
+
+git checkout $FROM; git pull; git checkout $TO; git merge $FROM
+
