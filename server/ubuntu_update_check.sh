3813e9c5c5dfd6e68b348f780b565e71e8b9fce5
okojj <puchon@gmail.com>
2024-10-06 19:27:10 +0900

    add new to check ubuntu updates

--- server/ubuntu_update_check.sh
+++ server/ubuntu_update_check.sh
@@ -8,5 +8,5 @@ update_count=$(cat $CHECK_FILE | grep "updates can be applied" | awk '{print $1}
 
 if [[ $update_count -gt 0 ]]; then
     echo "$update_count"
-    exit "$update_count"
+    exit "$update_count"    # for jenkins
 fi
