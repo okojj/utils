52556c21d488e80680890eb63282afa521bcf73c
OJJ <puchon@gmail.com>
2024-10-13 20:08:59 +0900

    add cat

--- server/ubuntu_update_check.sh
+++ server/ubuntu_update_check.sh
@@ -7,6 +7,7 @@ CHECK_FILE="/var/lib/update-notifier/updates-available"
 update_count=$(cat $CHECK_FILE | grep "updates can be applied" | awk '{print $1}')
 
 if [[ $update_count -gt 0 ]]; then
-    echo "$update_count"
+#    echo "$update_count"
+    cat $CHECK_FILE
     exit "$update_count"    # for jenkins
 fi
