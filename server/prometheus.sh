00aa7211e2bcd94265dab427e891f0ff582f7379
OJJ <puchon@gmail.com>
2025-01-26 11:51:49 +0000

    add command

--- server/prometheus.sh
+++ server/prometheus.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-sudo ocker run \
+# Create persistent volume for your data
+docker volume create prometheus-data
+# Start Prometheus container
+sudo docker run \
     -p 9090:9090 \
-    -v /path/to/prometheus.yml:/etc/prometheus/prometheus.yml \
+    -v ./prometheus.yml:/etc/prometheus/prometheus.yml \
+    -v prometheus-data:/prometheus \
     prom/prometheus
